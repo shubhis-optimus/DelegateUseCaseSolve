af9b80f6da92b0570446b922f2805d5c3b24cfac
shubhis-optimus <shubhi.srivastava@optimusinfo.com>
2025-04-17 18:05:01 +0530

    Delegate UseCase solved
    Made changes to private property of one class from another class using delegates.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using static DelegateUseCase.ClassOne;
+
+namespace DelegateUseCase
+{
+    
+
+    public class ClassOne
+    {
+        static void Main(string[] args)
+        {
+            var classOne = new ClassOne();
+            var classTwo = new ClassTwo();
+            ChangePropertyDelegate delegateMethod = classOne.SetMyProperty;
+            classOne.ShowValue();
+            classTwo.InvokeDelegate(delegateMethod);
+            classOne.ShowValue();
+        }
+        private int MyProperty { get; set; }
+        public void SetMyProperty(int value)
+        {
+            MyProperty = value;
+        }
+        public void ShowValue()
+        {
+            Console.WriteLine($"MyProperty = {MyProperty}");
+        }
+        public delegate void ChangePropertyDelegate(int value);
+
+    }
+    public class ClassTwo
+    {
+        public void InvokeDelegate(ChangePropertyDelegate delegateMethod)
+        {
+            int i = int.Parse(Console.ReadLine());
+            delegateMethod(i);
+        }
+         
+    }
+
+
+}
