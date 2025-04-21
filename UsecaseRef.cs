4f3cdf6c519b50c6f546e1f30380a00f4ac9bf6a
shubhis-optimus <shubhi.srivastava@optimusinfo.com>
2025-04-21 11:20:16 +0530

    Implement using ref keyword
    As discussed in the review , Tried to implement the very first approach I came up with , here I have used ref keyword for making changes to a private property from outside of the class .

---
+++ UsecaseRef.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateUseCase
+{
+    public class ClassOnee
+    {
+        private int key { get; set; } = 12;
+
+        public int GetKey()
+        {
+            return key;
+        }
+
+        public void SetKey(int value)
+        {
+            key = value;
+        }
+
+        public int PrintPrivateProperty()
+        {
+            return key;
+        }
+
+        public static void Main()
+        {
+            ClassTwoo obj2 = new ClassTwoo();
+            ClassOnee obj = new ClassOnee();
+            var before = obj.PrintPrivateProperty();
+            Console.WriteLine("Before: " + before);
+            var after = obj2.ChangePrivateProperty(obj);
+            Console.WriteLine("After: " + after);
+        }
+    }
+    public class ClassTwoo
+    {
+        public int ChangePrivateProperty(ClassOnee obj)
+        {
+            int change = int.Parse(Console.ReadLine());
+
+            obj.SetKey(change);
+            return obj.GetKey();
+        }
+    }
+
+}
